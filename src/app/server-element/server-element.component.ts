6bc5eba1daea0151aaeaefdf01d23efdefbc3a04
Robert Lewis <Robbysim37@gmail.com>
2023-12-30 00:58:12 -0500

    component relationships

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrl: './server-element.component.css'
 })
+
 export class ServerElementComponent {
-  element:{};
+  @Input("srvElement") element:{type: string, name: string, content: string};
 }
